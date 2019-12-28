b166470a0199af4a562401d52ae8315373e9c9dd
BeGIMATH <begatimi_95@outlook.com>
2019-12-28 14:19:23 +0100

    Implemented squential Cadane Algorithm both 1D and 2D

---
+++ cadane1D.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <limits>
+#include <eigen3/Eigen/Dense>
+
+using namespace Eigen;
+void kadane(const VectorXd &array, double &maxSum, int &l, int &r)
+{
+    maxSum = -std::numeric_limits<double>::infinity();
+    l = 0;
+    r = 0;
+    double sum = 0;
+    int currentStartIndex = 0;
+    for (int i = 0; i < array.size(); ++i)
+    {
+        sum += array(i);
+        if (sum > maxSum)
+        {
+            maxSum = sum;
+            l = currentStartIndex;
+            r = i;
+        }
+        if (sum < 0)
+        {
+            sum = 0;
+            currentStartIndex = i + 1;
+        }
+    }
+}
+
+int main()
+{
+    //Size of the array
+    int n = 10;
+    //An array  o size n filled with random numbers between (-1,1)
+    VectorXd v = VectorXd::Random(n);
+    std::cout << " [ " << v.transpose() << " ] " << std::endl;
+    for (int i = 0; i < v.size(); i++)
+    {
+        v(i) = static_cast<int>(10.0 * v(i));
+    }
+    double maxSum;
+    int left, right;
+    kadane(v, maxSum, left, right);
+    std::cout << " [ " << v.transpose() << " ] " << std::endl;
+    //std::cout << left << " " << right << " " << std::endl;
+    VectorXd V = VectorXd(right - left + 1);
+    for (int i = 0; i < V.size(); i++)
+    {
+        V(i) = v(i + left);
+    }
+    std::cout << maxSum << "  " << std::endl;
+    std::cout << "  " << left << "  " << right << "  " << std::endl;
+    std::cout << " [ " << V.transpose() << " ] " << std::endl;
+}
