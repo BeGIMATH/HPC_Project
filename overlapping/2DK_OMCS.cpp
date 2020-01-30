e572db16dfdf9eb2056a6e6d986b82424403ff64
EbbaEkblom <ebba@LAPTOP-54J88HA5.localdomain>
2020-01-30 21:10:16 +0100

    Fixed some errors

--- overlapping/2DK_OMCS.cpp
+++ overlapping/2DK_OMCS.cpp
@@ -1,65 +1,103 @@
 //Include all the right things...
 #include <algorithm> 
-#include <Eigen/Core>
 #include <vector>
+#include <limits>
+#include <iostream>
+using namespace std;
 
-using namespace Eigen;
-
-/*Implement*/
-void main(){
-    // Initialize n*n random matrix
-    // Call function to compute K_max
-}
-
-void 2DKmax(MatrixXd M, int K){
+void 2DKmax(vector<double> M, int K,int rows, int col){
+    // n is the number of columns??
+    int m = rows;
+    int n = col;
     // Set of all intervals (s,t) in increasing order
-    MatrixXd sortedPairs = findSortedCombinations(n);
-    MatrixXd F_w = findLeftConvex(M,sortedPairs);
-    MatrixXd F_n = findRightConvex(M,sortedPairs);
+    vector<int> sortedPairs = findSortedCombinations(m);
+    // Last input == 1 for left, ==0 for right
+    vector<double> F_w = findConvex(M,sortedPairs,K,1);
+    vector<double> F_n = findConvex(M,sortedPairs,K,0);
+    
     // Finilization: adding left and right convex shapes
-    MatrixXd F_wn;
-    for (int k=1;k<m;k++){
-        for(int s=0;s<n;s++){
-            for(int t=0;s<n;t++){
-                F_wn(k,s,t) = F_w(k,s,t) + F_n(k,s,t) - summarize(M,k,s,t);
+    vector<double> F_wn;
+    for (int k=1;k<n;k++){
+        for(int s=0;s<m;s++){
+            for(int t=0;t<m;t++){
+                // set a range of values to a 
+                int start = k*m*m*K + s*m*K + t*K;
+                int end = k*m*m*K+s*m*K + t*K + K;
+
+                //Herrejävlar, måste strukturera upp det här
+                vector<double> sub1(&F_w[start],&F_w[end]);
+                vector<double> sub2(&F_n[start],&F_n[end]);
+                vector<double> temp = add(sub1,sub2);
+                int tempint = summarize(M,k,s,t);
+                addInt(temp,summarize(M,k,s,t));
+
+                for(int j = 0;j<K;j++){
+                    F_w[k*m*m*K + s*m*K + t*K + j] = temp[j];
+                }
+                //std::fill(F_wn.begin() + start, F_wn.begin() + start + end,
+                          //addInt(add(sub1,sub2),summarize(M,k,s,t)));
             }
         }
     }
 }
 
 /* Finds all possible F_w*/
-MatrixXd findLeftConvex(MatrixXd M, MatrixXd sortedPairs){
+std::vector<double> findConvex(std::vector<double> M, std::vector<int> sortedPairs, int K, int type){
     int m = sizeof(M,1);
     int n = sizeof(M,2);
-    double F_w[m][n][n][K] = {0}; //=-infty
-    for(int k=1;k<m;k++){
-        // Find all intervals s-t in increasing order
+    int num_of_comb = numberOfCombinations(m);
+    std::vector<double> F_w(m*n*n*K,-std::numeric_limits<double>::infinity());
+
+    // set all k=0 values to 0 for sets s<= t
+    for (int i = 0; i<num_of_comb;i++){
+        int s = sortedPairs[i*2];
+        int t = sortedPairs[i*2+1];
+        F_w[0,s,t,0] = 0;
+    }
+    
+    // TODO
+    vector<int> k_range(K);
+    if (type == 1){ //left
+        int x = 0;
+        std::generate(k_range.begin(), k_range.end(), [&]{ return x++; }); 
+    }
+    else if (type == 0){
+        int x = K-1;
+        std::generate(k_range.begin(), k_range.end(), [&]{ return x--; }); 
+    }
+    
+    for(int k : k_range){
         for(int i=0;i<num_of_comb;i++){
-            int s = sortedPairs[i][1];
-            int t = sortedPairs[i][2];
-            F_w[k,s,t] = max(F_W(k-1,s,t)+summarize(M,k,s,t),
-                             F_w(k,s+1,t) + M(s,k),
-                             F_w(k,s,t+1) + a[t,k]);
+            int s = sortedPairs[i*2];
+            int t = sortedPairs[i*2+1];
+
+            int start = k*m*m*K + s*m*K + t*K;
+            int end = k*m*m*K + s*m*K + t*K + K;
+
+            int prev = (type==1) ? (k-1) : (k+1);
+            // Subvectors of F_w corresponding to the three cases
+            std::vector<double> L1(&F_w[prev*m*m*K + s*m*K + t*K],
+                                   &F_w[prev*m*m*K + s*m*K + t*K + K]);
+            addInt(L1,summarize(M,k,s,t));
+            std::vector<double> L2(&F_w[k*m*m*K + (s+1)*m*K + t*K],
+                                   &F_w[k*m*m*K + (s+1)*m*K + t*K + K]);
+            addInt(L2,M[s,k]);
+            std::vector<double> L3(&F_w[k*m*m*K + s*m*K + (t+1)*K],
+                                   &F_w[k*m*m*K + s*m*K + (t+1)*K + K]);
+            addInt(L3,M[t,k]);
+            vector<double> L_max = max(L1,L2,L3);
+            for(int j = 0;j<K;j++){
+                F_w[k*m*m*K + s*m*K + t*K + j] = L_max[j];
+            }
+            //std::fill(F_w.begin() + start, F_w.begin() + start + end, max(L1,L2,L3)); 
         }
     }
+    return F_w;
 }
 
-/* Finds all possible F_n,
-    might be unnecessary, could use the same function for F_n and F_w */
-MatrixXd findRightConvex(MatrixXd M,MatrixXd sortedPairs){
-    int m = sizeof(M,1);
-    int n = sizeof(M,2)
-    F_w = [m][n][n][K] = {0}; //=-infty
-    for(int k=m-2;k>=0;k--){
-        // Find all intervals s-t in increasing order
-        for(int i=0;i<num_of_comb;i++){
-            int s = sortedPairs[i][1];
-            int t = sortedPairs[i][2];
-            F_w[k,s,t] = max(F_W(k+1,s,t)+summarize(M,k,s,t),
-                             F_w(k,s+1,t) + M(s,k),
-                             F_w(k,s,t+1) + a[t,k]);
-        }
-    }
+
+int numberOfCombinations(int m){
+    int num_of_comb = m*(m+1)/2;
 }
 
 /**
@@ -67,17 +105,20 @@ MatrixXd findRightConvex(MatrixXd M,MatrixXd sortedPairs){
  * for all s<t
  * comb(i,j) = (s,t)
  **/
-int** findSortedCombinations(n){
-    int num_of_comb = n*(n+1)/2;
-    MatrixXd comb(num_of_comb,2);
+std::vector<int> findSortedCombinations(int m){
+    int num_of_comb = numberOfCombinations(m);
+    // 2D vector of size num_of_comb*2 with initial value 0
+    //std::vector<std::vector<int> > comb(num_of_comb,std::vector<int>(2, 0));
+    // Each set of two represents a combination of (s,t) for a given iteration (counter)
+    //int comb[num_of_comb*2];
+    std::vector<int> comb(num_of_comb*2);
     int counter = 0;
-    for(diff=0;diff<n-1;diff++){
-        for(t=diff;t<n;t++){
-            s = t-diff;
-            comb(counter,1) = s;
-            comb(counter,2) = t;
+    for(int diff=0;diff<m-1;diff++){
+        for(int t=diff;t<m;t++){
+            int s = t-diff;
+            comb[counter*2] = s;
+            comb[counter*2+1] = t;
             counter++;
-
         }
     }  
     return comb;
@@ -87,15 +128,36 @@ int** findSortedCombinations(n){
  * Summarizes column k of matrix M,
  * from s to t.
  **/
-int summarize(MatrixXd M,int k,int s,int t){
-    for(i=s;i<=t;i++){
-        double sum = sum + M(i,k);
+int summarize(vector<double> M,int k,int s,int t){
+    
+    for(int i=s;i<=t;i++){
+        double sum = sum + M[i*m + k];
     }
 }
 
 /*From K-tuple L_1,...,L_m max(L_1,...) returns a tuple with the K largest elements*/
-MatrixXd max(MatrixXd L1, MatrixXd L2, MatrixXd L3){
-    
+vector<double> max(vector<double> L1, vector<double> L2, vector<double> L3){
+    // merge matrices and perform quicksort?
+    return;
 }
 
+void addInt(vector<double> &v, int a){
+    for (int i = 0;i<v.size();i++){
+        v[i] = v[i]+a;
+    }
+}
 
+vector<double> add(vector<double> v1, vector<double> v2){
+    vector<double> v3;
+    for(int i = 0; i<v1.size();i++){
+        v3.push_back(v1[i] + v2[i]);
+    }
+    return v3;
+}
+
+/*Implement*/
+int main(){
+    // Initialize n*n random matrix
+    // Call function to compute K_max
+    return 0;
+}
